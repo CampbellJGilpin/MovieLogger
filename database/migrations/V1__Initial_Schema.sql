94fbb581af3e9fff2791892848c5843e8ba0159c
CampbellJGilpin <campbell.gilpin@unosquare.com>
2025-04-24 14:34:19 +0100

    Update Schema for postgres

--- database/migrations/V1__Initial_Schema.sql
+++ database/migrations/V1__Initial_Schema.sql
@@ -1,10 +1,10 @@
 CREATE TABLE Genres (
-    Id INT PRIMARY KEY,
+    Id SERIAL PRIMARY KEY,
     Title VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE Users (
-    Id INT PRIMARY KEY,
+    Id SERIAL PRIMARY KEY,
     UserName VARCHAR(100) NOT NULL,
     Email VARCHAR(255) UNIQUE NOT NULL,
     Password VARCHAR(255) NOT NULL,
@@ -13,38 +13,38 @@ CREATE TABLE Users (
 );
 
 CREATE TABLE Movies (
-    Id INT PRIMARY KEY,
+    Id SERIAL PRIMARY KEY,
     Title VARCHAR(500) NOT NULL,
-    Description VARCHAR(500),
-    ReleaseDate DATETIME,
+    Description TEXT,
+    ReleaseDate TIMESTAMP,
     GenreId INT NOT NULL,
     IsDeleted BOOLEAN DEFAULT FALSE,
     CONSTRAINT FK_Movies_GenreId_Genres_Id FOREIGN KEY (GenreId) REFERENCES Genres(Id)
 );
 
 CREATE TABLE UserMovies (
-    Id INT PRIMARY KEY,
+    Id SERIAL PRIMARY KEY,
     UserId INT NOT NULL,
     MovieId INT NOT NULL,
     Favourite BOOLEAN DEFAULT FALSE,
     OwnsMovie BOOLEAN DEFAULT FALSE,
-    UpcomingViewDate DATETIME,
+    UpcomingViewDate TIMESTAMP,
     CONSTRAINT FK_UserMovies_UserId_Users_Id FOREIGN KEY (UserId) REFERENCES Users(Id),
     CONSTRAINT FK_UserMovies_MovieId_Movies_Id FOREIGN KEY (MovieId) REFERENCES Movies(Id),
     UNIQUE(UserId, MovieId)
 );
 
 CREATE TABLE Viewings (
-    Id INT PRIMARY KEY,
+    Id SERIAL PRIMARY KEY,
     UserMovieId INT NOT NULL,
-    DateViewed DATETIME NOT NULL,
+    DateViewed TIMESTAMP NOT NULL,
     CONSTRAINT FK_Viewings_UserMovieId_UserMovies_Id FOREIGN KEY (UserMovieId) REFERENCES UserMovies(Id)
 );
 
 CREATE TABLE Reviews (
-    Id INT PRIMARY KEY,
+    Id SERIAL PRIMARY KEY,
     ViewingId INT NOT NULL,
-    ReviewText VARCHAR(500),
+    ReviewText TEXT,
     Score INT,
     CONSTRAINT FK_Reviews_ViewingId_Viewings_Id FOREIGN KEY (ViewingId) REFERENCES Viewings(Id)
 );
