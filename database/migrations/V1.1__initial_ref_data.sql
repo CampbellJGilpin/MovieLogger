1efae05f39b24fb2294b6057a645dc73de107ac4
CampbellJGilpin <campbell.gilpin@unosquare.com>
2025-04-24 15:27:13 +0100

    Updated migration for ref data

---
+++ database/migrations/V1.1__initial_ref_data.sql
@@ -0,0 +1,15 @@
+INSERT INTO Genres(Id, Title) 
+VALUES 
+(1, 'Action'), 
+(2, 'Horror'), 
+(3, 'Drama'), 
+(4, 'Comedy'), 
+(5, 'Thriller'), 
+(6, 'Romance'), 
+(7, 'Science Fiction'),
+(8, 'Western'),
+(9, 'Documentary'),
+(10, 'Family'),
+(11, 'Musical'),
+(12, 'Fantasy'),
+(13, 'War');
